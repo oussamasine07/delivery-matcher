d6b1b02795e8cf2991650e4ccba429f96aa8a6cd
oussamasine07 <sineoussama@gmail.com>
2025-06-21 16:29:08 +0100

    show journies from database

--- frontend/src/app/components/pages/journy/journy-index/journy-index.component.ts
+++ frontend/src/app/components/pages/journy/journy-index/journy-index.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {Journy} from '../../../../models/interfaces/journy';
+import {JournyService} from '../../../../services/journy/journy.service';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-journy-index',
-  imports: [],
+  imports: [
+    NgFor
+  ],
   templateUrl: './journy-index.component.html',
   styleUrl: './journy-index.component.css'
 })
-export class JournyIndexComponent {
+export class JournyIndexComponent implements OnInit {
+
+  journyService: JournyService = inject(JournyService);
+
+  journies: Journy[] = [];
+
+  ngOnInit() {
+    this.journyService.getAllJourniesByDriverId().subscribe({
+      next: (jrs: Journy[]) => {
+        this.journies = jrs;
+      },
+      error: e => {
+        console.log(e);
+      }
+    })
+  }
 
 }
